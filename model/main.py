26607a5bacb8ab4ab11194bc8dcfd4aa24f279c2
Anuj Singh <anuj.as828@gmail.com>
2024-09-07 16:11:38 +0530

    model

---
+++ model/main.py
@@ -0,0 +1,14 @@
+import pymupdf
+
+#Here we will put the incoming document for processing
+doc = pymupdf.open('datasets/abc.pdf')
+# page = doc.load_page(0)
+# text = page.get_text()
+
+for page in doc:
+    text = page.get_text()
+    print(text)
+
+
+
+# print(text)
